65a7dde203282b8e454d89391608a7d8a41a620f
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-13 23:08:07 +0000

    [Add]Topページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ルーティングを記述（どのURLにアクセスした時に、どのコントローラのどのアクションを呼び出すか）
+  # HTTPメソッド 'URL' => 'コントローラ#アクション'
+
+  #URL「top」にアクセスした時にhomesコントローラのtopアクションが呼び出される
+  get 'top' => 'homes#top'
+
 end
